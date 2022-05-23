2d3479348fea9609ae1c8d358a62e8a3c124a703
akiyama-prog <autumn56y@gmail.com>
2022-05-23 17:10:42 +0100

    created jobs table and model

---
+++ database/migrations/2022_05_23_115401_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id()->index();
+            $table->foreignId('candidate_id')->constrained();
+            $table->string('job_title', 50);
+            $table->string('company_name', 50);
+            $table->dateTime('started_at');
+            $table->dateTime('ended_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
